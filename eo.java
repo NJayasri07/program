a8bcc26cf63d55e12032e64774c705ae35d40273
NJayasri07 <52317432+NJayasri07@users.noreply.github.com>
2019-07-15 11:47:45 +0530

    Create eo.java

---
+++ eo.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class eo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        else
+            System.out.println(num + " is odd");
+    }
+}
